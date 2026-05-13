ef20997b4ca4cce04fa6ff70f3246a82aeb32f8b
kazmi826 <kazmiizan668@gmail.com>
2026-05-14 00:31:15 +0500

    fix 171 broken formulas all tools working

---
+++ scripts/check_formulas.py
@@ -0,0 +1,13 @@
+﻿import json
+with open('tools_database.json', encoding='utf-8') as f:
+    db = json.load(f)
+with open('../assets/js/calc.js', encoding='utf-8') as f:
+    js = f.read()
+missing = []
+for t in db['tools']:
+    formula = t.get('formula', '')
+    if formula and formula not in js:
+        missing.append(t['slug'] + ' -> ' + formula)
+print('Missing formulas:', len(missing))
+for m in missing:
+    print(' ', m)
